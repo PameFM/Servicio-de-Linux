38e3827a5c7b960e63d8acaa9d8735b5c3c6bfa4
PameFM <140655349+PameFM@users.noreply.github.com>
2025-05-11 15:48:13 -0600

    Correccion de la prueba anterior

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  res.send('Funcionó')
+  res.send('Hello World!<br>Funcionó')
 })
 
 app.listen(port, () => {
